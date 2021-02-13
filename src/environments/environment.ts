efcdd46eb4c19532e9f7339ec967923f724ad29a
Rendy Abu Zubair <37995990+AbuZubair@users.noreply.github.com>
2021-02-13 19:40:08 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  key : 'AAAAPcydNA0:APA91bFr18GykidxKI29Z0mFVN7fx3w8j5gsnEqNy97MPnFQUCujLz-5xT-yruO0rUhmibLry3vxUFVynV4Bg1I00iuMA0WfQmLVBSmgFAfzIg9TjvZMPpho_sdMEW9L0MNfjNcXk7LC',
+  key : 'AAAAxxxxxxxxxxxxxxxxxxxxxxxxxx',
   fcmUrl : "https://fcm.googleapis.com/fcm/send"
 };
 
